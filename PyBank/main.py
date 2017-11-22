07e3695e5528d7d27bfdb83b3d918e90d7bf26af
Andrea Kwong <andrea.kwong@gmail.com>
2017-11-21 23:32:47 -0800

    completed homework assignment
    finish first Python assignment - PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,52 +1,84 @@
+#PyBank Assignment
+
+#import modules
 import os
 import csv
 
-#allbudget = []
+#define date and revenue lists
 date = []
 revenue = []
-#budget1_csv_path = os.path.join("data","budget_data_1.csv")
 
+#read in csv file and append each column into lists
 with open("budget_data_1.csv", newline= '') as file1:
-    budget1file = csv.reader(file1, delimiter= ',')
-    next(budget1file)
-    for lines in budget1file:
+    budgetfile = csv.reader(file1, delimiter= ',')
+    next(budgetfile)
+    for lines in budgetfile:
         date.append(lines[0])
         revenue.append(int(lines[1]))
-    #print(date)
-with open("budget_data_2.csv", newline= '') as file2:
-    budget2file = csv.reader(file2, delimiter= ',')
-    next(budget2file)
-    for line in budget2file:
-        date.append(line[0])
-        revenue.append(int(line[1]))
-print(len(date))
-
-#total months
 #print(len(date))
-#dateSet = set(date)
-#print(len(set(date)))
-unique_date = []
+
+#check that each month is unique and get the total number of months
+#--------TOTAL NUMBER OF MONTHS (calc)
+unique_count_months = []
 for x in date: 
-    if x not in unique_date:
-        unique_date.append(x)
+    if x not in unique_count_months:
+        unique_count_months.append(x)
     else:
         continue
-print(len(unique_date))
-        
+#print(len(unique_count_months))
+#print(int(len(unique_count_months)-1))
+
+#--------TOTAL REVENUE (calc)
 #total revenue
-totalrev = 0
+total_rev = 0
 for x in revenue:
-    totalrev = totalrev + x
-print(totalrev)
+    total_rev = total_rev + x
+#print(total_rev)
 
-averagerev = totalrev/(len(date))
-print(averagerev)
+#function to get differences in revenue for each month
+def rev_diff(prev_month,curr_month):
+    return(curr_month - prev_month)
 
-#b = {allbudget[i]: allbudget[i+1] for i in range(0, 1)}
-#print(b)
-#total counts
-#total_months = 0
-#for i in allbudget:
-#    
+#apply function to get a list of differences in revenue between months
+total_rev_diff = []
+for z in range(len(revenue)):
+    if z == len(revenue) - 1:
+        break
+    diff = rev_diff(revenue[z],revenue[z+1])
+    total_rev_diff.append(diff)
+#print(total_rev_diff)
 
-#    print(i[0])
+
+#---------AVERAGE CHANGE IN REVENUE (calc)
+avg_rev_change = int(sum(total_rev_diff)/len(revenue))
+
+#insert 0 to the 'all_rev_diff' so that when combined with 'date' list, total rows will match
+#use zip function to get a list of tuples
+total_rev_diff.insert(0,0)
+combined_data = zip(date,total_rev_diff)
+
+#convert list of tuples to dictionary
+combined_data_dict = dict(combined_data)
+#print(combined_data_dict)
+
+
+#---------GREATEST INCREASE in REVENUE (calc)
+max_value = max(combined_data_dict.values())
+max_date = max(combined_data_dict, key=combined_data_dict.get)
+
+#---------GREATEST DECREASE in REVENUE (calc)
+min_value = min(combined_data_dict.values())
+min_date = min(combined_data_dict, key=combined_data_dict.get)
+
+
+
+#---------SUMMARY OF RESULTS
+print('')
+print("Financial Analysis")
+print("-----------------------------------")
+print("Total Months: " + str(len(unique_count_months)))
+print("Total Revenue:  " + "$" +str(total_rev))
+print("Average Revenue Change:  " + "$" +str(avg_rev_change))
+print("Greatest Increase in Revenue:  " + max_date + " ($" + str(max_value) + ")")
+print("Greatest Decrease in Revenue:  " + min_date + " ($" + str(min_value) + ")")
+print('')
