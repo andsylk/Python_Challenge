825c73b4a39c2f81468e74f9b2cbaf9df93e2ffc
Andrea Kwong <andrea.kwong@gmail.com>
2017-11-22 11:02:50 -0800

    some edits

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,8 +6,9 @@ import csv
 #create candidates list
 candidates = []
 
+
 #read in csv file and append relevant column into list
-with open('election_data_1.csv', newline = '') as file:
+with open('election_data_2.csv', newline = '') as file:
     election_data = csv.reader(file, delimiter = ',')
     next(election_data)
     for line in election_data:
@@ -25,8 +26,6 @@ for x in candidates:
         candidate_names.append(x)
     else:
         continue
-#print(candidate_names[0])
-#print(candidate_names)
 
 #get each candidates votes and percentage of votes
 candidate_votes = []
@@ -40,29 +39,28 @@ candidate1_perc = round((candidate1_votes/total_votes)*100,2)
 candidate2_votes = candidates.count(candidate_names[1])
 candidate_votes.append(candidate2_votes)
 candidate2_perc = round((candidate2_votes/total_votes)*100,2)
-#print(candidate_names[1] + " has " + str(candidate2_votes) + " votes.")
+
 
 #Candidate 3
 candidate3_votes = candidates.count(candidate_names[2])
 candidate_votes.append(candidate3_votes)
 candidate3_perc = round((candidate3_votes/total_votes)*100,2)
-#print(candidate_names[2] + " has " + str(candidate3_votes) + " votes.")
+
 
 #Candidate 4
 candidate4_votes = candidates.count(candidate_names[3])
 candidate_votes.append(candidate4_votes)
 candidate4_perc = round((candidate4_votes/total_votes)*100,2)
-#print(candidate_names[3] + " has " + str(candidate4_votes) + " votes.")
+
 
 #combine candidate names and candidate votes into dictionary
 candidate_names_votes = {}
 for i in range(len(candidate_names)):
     candidate_names_votes[candidate_names[i]] = candidate_votes[i]
-#print(candidate_names_votes)
 
 #get the winning candidate
 winning_candidate = max(candidate_names_votes, key=candidate_names_votes.get)
-#print(winning_candidate)
+
 
 
 #---------SUMMARY OF RESULTS
